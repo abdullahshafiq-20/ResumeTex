d669e0707c4aa8439bc3cf07926acd079ecd76b7
Abdullah Shafiq <111144443+abdullahshafiq-20@users.noreply.github.com>
2025-09-03 11:29:56 +0500

    Update BeforeAfterPage.jsx

--- client/src/pages/BeforeAfterPage.jsx
+++ client/src/pages/BeforeAfterPage.jsx
@@ -18,7 +18,7 @@ const BeforeAfterPage = () => {
   const [deploymentLog, setDeploymentLog] = useState({ type: "", message: "" });
   const navigate = useNavigate();
   const [count, setCount] = useState(0);
-  const [disable, setDisable] = useState(true);
+  const [disable, setDisable] = useState(false);
   const [showNewVersionModal, setShowNewVersionModal] = useState(false);
 
   // Status styles configuration
